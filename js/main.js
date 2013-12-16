5765c87dea0b98fa72d60b6cb5b79a45679e7d6d
Guillaume Coste <guillaume.coste@livingobjects.fr>
2013-12-16 17:24:43 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*var mopidy = new Mopidy;
+
+$(document).ready(function(){
+	var buttonPlay = $('#button-play');
+	var buttonNext = $('#button-next');
+	var buttonPrev = $('#button-prev');
+
+	$('#form-search').submit(function(e){
+		e.preventDefault();
+		return false;
+	});
+
+	buttonPlay.click(function(){
+		if(buttonPlay.hasClass('fa-play')) {
+			mopidy.pause();
+			buttonPlay.removeClass('fa-play');
+			buttonPlay.addClass('fa-pause');
+		} else {
+			mopidy.play();
+			buttonPlay.removeClass('fa-pause');
+			buttonPlay.addClass('fa-play');
+		}
+	});
+});*/
